28265dd9a329967e6c0107574c6289ba04753ad0
Julius Hietala <julius@smartly.io>
2020-10-25 12:09:30 +0200

    Data gen improve

--- generate/rlkit_data_generation.py
+++ generate/rlkit_data_generation.py
@@ -15,7 +15,7 @@ class SidewaysActor(object):
         self.current_action_idx = 0
 
     def get_action(self, obs, **kwargs):
-        ac = np.array([0.0,0.0,0.0,0.0])
+        ac = np.array([0.0,0.0,0.0])
         if self.current_action_idx < len(self.items):
             action = copy.deepcopy(self.items[self.current_action_idx])
             #action.append(1)
@@ -27,9 +27,8 @@ class SidewaysActor(object):
             ac[0] = -0.05
             ac[2] = -0.5
 
-        ac = np.array([-0.1,0.1,0.1, 0.0])
-        if self.current_action_idx > 30:
-            ac[3] = -0.1
+        #ac = np.array([-0.1,0.1,0.1])
+
         #ac = np.array([0,0,0])
         self.current_action_idx += 1
         #ac += np.random.normal(0, 0.1, 3)
@@ -103,18 +102,21 @@ if __name__ == "__main__":
 
     env_name = 'Cloth-v1'
     env_kwargs = dict(
-        learn_grasp = False,
-        n_actions=4,
         task="sideways",
-        pixels=False,
+        pixels=True,
+        debug_render=True,
+        rgb=True,
+        image_size=500,
         strict=True,
         distance_threshold=0.05,
-        randomize_params=True,
-        uniform_jnt_tend=True
+        randomize_params=False,
+        randomize_geoms=False,
+        max_advance=0.05,
+        random_seed=1
 
     )
     num_examples = 100
-    rollouts = make_demo_rollouts(env_name, num_examples, "sideways", True, **env_kwargs)
+    rollouts = make_demo_rollouts(env_name, num_examples, "sideways", render=False, **env_kwargs)
     file_name = "rlkit_demo_data"
     file_name += "_" + str(num_examples)
     file_name += "_" + env_name
