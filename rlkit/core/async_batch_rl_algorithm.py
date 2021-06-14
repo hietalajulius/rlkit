5f63dbc8a33b41ed8033ef574ac005b848e93fca
Julius Hietala <julius.hietala@gmail.com>
2021-06-14 17:53:43 +0300

    update async train

--- rlkit/core/async_batch_rl_algorithm.py
+++ rlkit/core/async_batch_rl_algorithm.py
@@ -14,34 +14,35 @@ class AsyncBatchRLAlgorithm(BaseRLAlgorithm, metaclass=abc.ABCMeta):
     def __init__(
             self,
             trainer,
+            save_folder,
             batch_size,
             max_path_length,
             num_epochs,
-            num_eval_rollouts_per_epoch,
             num_expl_steps_per_train_loop,
             num_trains_per_train_loop,
-            evaluation_data_collector,
-            preset_evaluation_data_collector,
             num_collected_steps,
             buffer_memory_usage,
             collector_memory_usage,
             env_memory_usages,
-            num_eval_param_buckets=1,
-            save_policy_every_epoch=1,
+            demo_data_collector = None,
+            num_demos= 0,
             num_train_loops_per_epoch=1,
             min_num_steps_before_training=0,
-            demo_paths=None,
+            save_policy_every_epoch=1,
+            script_policy=None,
+            eval_suite=None,
             batch_queue=None,
             policy_weights_queue=None,
             new_policy_event=None,
             batch_processed_event=None,
-            debug_same_batch=False
     ):
         super().__init__(
             trainer,
-            evaluation_data_collector=evaluation_data_collector,
-            preset_evaluation_data_collector=preset_evaluation_data_collector
-
+            None,
+            None,
+            None,
+            None,
+            None,
         )
         self.train_collect_ratio = 4
         self.num_collected_steps = num_collected_steps
@@ -55,26 +56,22 @@ class AsyncBatchRLAlgorithm(BaseRLAlgorithm, metaclass=abc.ABCMeta):
         self.batch_size = batch_size
         self.max_path_length = max_path_length
         self.num_epochs = num_epochs
-        self.num_eval_rollouts_per_epoch = num_eval_rollouts_per_epoch
         self.num_trains_per_train_loop = num_trains_per_train_loop
         self.num_train_loops_per_epoch = num_train_loops_per_epoch
         self.num_expl_steps_per_train_loop = num_expl_steps_per_train_loop
         self.min_num_steps_before_training = min_num_steps_before_training
         self.save_policy_every_epoch = save_policy_every_epoch
-        self.num_eval_param_buckets = num_eval_param_buckets
-        self.debug_same_batch = debug_same_batch
+
+        self.num_demos = num_demos
+
+        self.save_folder = save_folder
+        self.script_policy = script_policy
+        self.eval_suite = eval_suite
+        self.demo_data_collector = demo_data_collector
+
 
     def _train(self):
         start_time = time.time()
-        if self.min_num_steps_before_training > 0:
-            init_expl_paths = self.expl_data_collector.collect_new_paths(
-                self.max_path_length,
-                self.min_num_steps_before_training,
-                discard_incomplete_paths=False,
-            )
-            self.replay_buffer.add_paths(init_expl_paths)
-            self.expl_data_collector.end_epoch(-1)
-
         temp_policy_weights = copy.deepcopy(
             self.trainer._base_trainer.policy.state_dict())
         self.policy_weights_queue.put(temp_policy_weights)
@@ -83,55 +80,16 @@ class AsyncBatchRLAlgorithm(BaseRLAlgorithm, metaclass=abc.ABCMeta):
         print("Initialized policy")
         process = psutil.Process(os.getpid())
 
-        for epoch in gt.timed_for(
-                range(self._start_epoch, self.num_epochs),
-                save_itrs=True,
-        ):
-            if epoch % self.save_policy_every_epoch == 0:
-                torch.save(
-                    self.trainer._base_trainer.policy.state_dict(), 'async_policy/current_policy.mdl')
-                torch.save(
-                    self.trainer._base_trainer.alpha_optimizer.state_dict(), 'async_policy/current_alpha_optimizer.mdl')
-                torch.save(
-                    self.trainer._base_trainer.policy_optimizer.state_dict(), 'async_policy/current_policy_optimizer.mdl')
-                torch.save(
-                    self.trainer._base_trainer.qf1_optimizer.state_dict(), 'async_policy/current_qf1_optimizer.mdl')
-                torch.save(
-                    self.trainer._base_trainer.qf2_optimizer.state_dict(), 'async_policy/current_qf2_optimizer.mdl')
-
-                print("Saved current policy")
-
-            files = glob.glob('success_images/*')
-            for f in files:
-                os.remove(f)
-
-            self.eval_data_collector.collect_new_paths(
-                self.max_path_length,
-                self.num_eval_rollouts_per_epoch
-            )
-
-            self.preset_eval_data_collector.collect_new_paths(
-                self.max_path_length,
-                self.num_eval_param_buckets
-            )
-            gt.stamp('evaluation sampling')
-
-            eval_paths = self.eval_data_collector.get_epoch_paths()
-            print("EVAL SUCCESS RATRE", eval_util.get_generic_path_information(
-                eval_paths)['env_infos/final/is_success Mean'])
-
+        for epoch in range(self._start_epoch, self.num_epochs):
             print("Epoch", epoch)
             for cycle in range(self.num_train_loops_per_epoch):
-
-                # TODO: Use for memory debug
-                #print("Memory usage in train",process.memory_info().rss/10E9, "GB")
                 train_steps = epoch*self.num_train_loops_per_epoch * \
                     self.num_trains_per_train_loop + cycle*self.num_trains_per_train_loop
 
                 while train_steps > self.train_collect_ratio * self.num_collected_steps.value:
-                    print("Waiting collector to catch up...",
+                    print("Waiting collectors to catch up...",
                           train_steps, self.num_collected_steps.value)
-                    time.sleep(3)
+                    time.sleep(10)
 
                 start_cycle = time.time()
                 self.training_mode(True)
@@ -185,7 +143,6 @@ class AsyncBatchRLAlgorithm(BaseRLAlgorithm, metaclass=abc.ABCMeta):
                       cycle_time/self.num_trains_per_train_loop
                       )
 
-                gt.stamp('training', unique=False)
                 self.training_mode(False)
                 self.trainer._base_trainer.policy.vers += 1
 
